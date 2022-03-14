2f75835f4012636e1984695c7b63fa8c0fd18ba8
umangsaxena96 <umangsaxena1409@gmail.com>
2022-03-14 23:50:59 +0530

    First commit

---
+++ eclipse-workspace/LockerMePt1/src/locker/ClientApp.java
@@ -0,0 +1,16 @@
+
+
+package locker;
+
+import screens.LockerMe;
+
+public class ClientApp {
+
+    public static void main(String[] args) {
+    	
+    	LockerMe welcome = new LockerMe();
+    	welcome.introWS();
+    	welcome.GetUserInput();
+
+    }
+}
